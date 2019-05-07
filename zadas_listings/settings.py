e6683777b5a75e68a293d15c55a6ef361c055817
JQ <test@jq.com>
2019-05-07 14:42:47 -0700

    Installed Postges and migrated the default 15 errors.

--- zadas_listings/settings.py
+++ zadas_listings/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'zadas_listings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zadaslistings',
+        'USER': 'postgres',
+        'PASSWORD': 'neon90210',
+        'HOST': 'localhost',
     }
 }
 
