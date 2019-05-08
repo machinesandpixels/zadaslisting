b342e41b96f5720f60e9326801e951e46d279b22
JQ <test@jq.com>
2019-05-08 10:02:45 -0700

    Added models.CharField to the title property in the Listing Model

--- listings/models.py
+++ listings/models.py
@@ -5,7 +5,7 @@ from realtors.models import Realtor
 # Create your models here.
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
-    title = models.ForeignKey(max_length=200)
+    title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
