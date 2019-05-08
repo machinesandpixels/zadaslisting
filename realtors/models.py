75f807f2a8045e468ec3e6b0392a6242dd413e85
JQ <test@jq.com>
2019-05-08 14:41:30 -0700

    Migrated Listings and Realtors model to Postgres

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=20)
+    seller_of_the_month = models.BooleanField(default=False)
+    hire_date = models.DateField(datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name 
+
+
