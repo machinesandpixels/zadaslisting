b6882ab5ed9d8414454f604c9c3437cedc988c7f
JQ <test@jq.com>
2019-06-05 14:46:42 -0700

    Changed settings.py: 06/05/19

--- zadas_listings/settings.py
+++ zadas_listings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pxgnyzfgjj6&adu1t*m36q7^%#^3e*zcuhkw^986m$1-8u2s$4'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
