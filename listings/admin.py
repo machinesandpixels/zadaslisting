703a7dea81bee604949ba0efe8e7a1c42b1dd411
JQ <test@jq.com>
2019-06-05 09:53:49 -0700

    Added display fields and search_fields to both listings and and realtors page.

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+
+    # Have to add a comma or else it doesnt load
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode',
+    'price')
+
+    list_per_page = 10
+
+
+admin.site.register(Listing, ListingAdmin)
